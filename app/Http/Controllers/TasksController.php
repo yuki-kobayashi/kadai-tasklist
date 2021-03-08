12cabaa28b857ef74c99bfdc86580266d94630d7
yuki.kobayashi <ready-steady-go.reno@way.ocn.ne.jp>
2021-03-08 13:13:50 +0000

    add status column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -47,8 +47,15 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+        ]);
+        
         // タスクを作成
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -99,9 +106,16 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+        ]);
+        
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         // メッセージを更新
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,9 @@
         </header>
 
         <div class="container">
+            {{-- エラーメッセージ --}}
+            @include('commons.error_tasks')
+            
             @yield('content')
         </div>
 
